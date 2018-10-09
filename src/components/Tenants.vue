760285f6e17b211748dd97352237a6cee5cf4df6
Johan Blom <jblom@flextrade.com>
2018-10-09 12:07:09 +0100

    now using rxfire

--- src/components/Tenants.vue
+++ src/components/Tenants.vue
@@ -11,6 +11,8 @@
 
 <script>
 import db from '@/firebase/init'
+import { collectionData } from 'rxfire/firestore'
+import { tap } from 'rxjs/operators'
 export default {
   name: 'tenants',
   data() {
@@ -20,22 +22,31 @@ export default {
       }
   },
   created() {
-    db.collection('tenants').get().then( snapshot => {
+    const tenantsRef = db.collection('tenants');
+    collectionData(tenantsRef, 'id')
+      .pipe(
+        tap(tenants => console.log('This is just an observable!'))
+      )
+      .subscribe(snapshot => {
           snapshot.forEach(doc => {
-              let tenant = doc.data();
-              tenant.id = doc.id;
-              let plots = doc.data().plots;
-              if (plots && plots.length > 0) {
-                let i = 0;
-                for (i = 0; i < plots.length; i++) {
-                  plots[i].get().then(doc => {
-                    console.log(doc.id, doc.data().size)
-                  })
-                  }
+              console.log(doc);
+              if ((this.tenants.filter(tenant => tenant.id === doc.id)).length == 0) {
+                console.log('new tenant identified ', doc.type)
+                let tenant = doc;
+                tenant.id = doc.id;
+                let plots = doc.plots;
+                if (plots && plots.length > 0) {
+                  let i = 0;
+                  for (i = 0; i < plots.length; i++) {
+                    plots[i].get().then(doc => {
+                      console.log(doc.id, doc.data().size)
+                    })
+                    }
+                }
+                this.tenants.push(tenant);
               }
-              this.tenants.push(tenant);
+          })
       })
-    })
   }
 }
 </script>
